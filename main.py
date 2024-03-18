2b413efc084ba40573c15a6038965493373dfc58
dev_delphin <eduard.zakirov.96@mail.ru>
2024-03-18 19:16:54 +0500

    modernization

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-import timer as t
+import pomodoro
 
-def inputs(concentration_time, smal_break_time, large_break_time, cicles) -> t.timer_time:
-    variables = [concentration_time, smal_break_time, large_break_time, cicles]
+def inputs(concentration_time, smal_break_time, large_break_time, cicles) -> any:
+    variables = [concentration_time, smal_break_time , large_break_time, cicles]
     ifs = [45, 10, 30, 100]
     input_texts = ["время таймера в минутах: ", "время короткого перерыва в минутах: ", "время большого перерыва в минутах: ", "количество повторений: "]
     information_texts = ["концентрации не более 45 минут.", "короткого перерыва не более 10 минут.", "большого перерыва не более 30 минут.", ""]
-    for (key, vari), ifss, input_text, information_text in zip(enumerate(variables), ifs, input_texts, information_texts):
+    for val, ifss, input_text, information_text in zip(variables, ifs, input_texts, information_texts):
         while True:
             try:
-                # разобраться со списками
-                print(key, vari)
-                #print (variables[vari])
-                variables[key] = int(input(f"Установите {input_text}"))
-                print (vari)
-                if vari > ifss: 
+                variables.remove(0)
+                variables.insert(val, int(input(f"Установите {input_text}")))
+                if val > ifss: 
                     print(f"Рекомендуемое время {information_text}")
                     continue
                 break
             except ValueError:
                 print(f"Некорректный ввод. Попробуйте снова.")
-    #for key, val in enumerate(variables):
-    #    print (key)
-    print(concentration_time, smal_break_time, large_break_time, cicles)
-    #t.timer_time(concentration_time, smal_break_time, large_break_time, cicles)
+    pomodoro.timer_time(variables)
 
-#variables[variables.index(vari)]
 if __name__ == "__main__":
     inputs(concentration_time = 0, smal_break_time = 0, large_break_time = 0, cicles = 0)
--- pomodoro.py
+++ pomodoro.py
@@ -3,15 +3,22 @@ from datetime import datetime, timedelta
 from os import system
 from playsound import playsound
 
-def timer_time (concentration_time, smal_break_time, large_break_time, cicles) -> print:
+def timer_time (variables) -> print:
     to_break = 1
+    for key, val in enumerate(variables):
+        if key == 0: cicles = val
+        if key == 1: large_break_time = val
+        if key == 2: smal_break_time = val
+        if key == 3: concentration_time = val
+        
     while cicles > 0:
         data = [concentration_time, smal_break_time, large_break_time]
         texts = [f'Время концентрации. Минут: {concentration_time}.', f'Небольшой перерыв. Минут: {smal_break_time}.', f'Большой перерыв. Минут: {large_break_time}.']
         for date, text in zip(data,texts):
+            if date == smal_break_time and to_break >= 3 and text == f'Небольшой перерыв. Минут: {smal_break_time}.':continue
             if date == large_break_time and to_break < 3 and text == f'Большой перерыв. Минут: {date}.': continue
             cur_time = datetime.now()
-            date = cur_time + timedelta(minutes=date)
+            date = cur_time + timedelta(seconds=date)
             playsound('media/8bit.mp3',block = False)
             while date >= cur_time:
                 cur_time = datetime.now()
